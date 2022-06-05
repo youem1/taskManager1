b0c10a166afd0c4d64742c26b63249c579c03f82
you em <youem850@gmail.com>
2022-06-05 16:17:11 +0430

    it is 3 update

--- one/src/components/Profile.js
+++ one/src/components/Profile.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './profile.css';
 
 
 const Profile=()=>{
+    const [showSaveBtn,setShowSaveBtn]=useState(false);
+
+    const change_name=()=>{
+        setShowSaveBtn(true)
+
+    }
+
+    const change_password=()=>{
+        setShowSaveBtn(true)
+
+    }
 
     return(
         <div className="allProfile">
@@ -16,9 +27,10 @@ const Profile=()=>{
             </div>
 
             <div className="bodyProfile">
-                <div><span className='keyProfile'>name:</span ><span className='valueProfile'><input type="text" defaultValue={"yousef"}/></span></div>
-                <div><span className='keyProfile'>password:</span><span className='valueProfile'><input type="text"  defaultValue={"yousef"}/></span></div>
-                <div><span className='keyProfile'>E-mail:</span><span className='valueProfile'> <input type="text" defaultValue={"yousef"}/></span></div>
+                <div><span className='keyProfile'>name:</span ><span onChange={change_name} className='valueProfile'><input type="text" defaultValue={"yousef"}/></span></div>
+                <div><span className='keyProfile' >password:</span><span onChange={change_password} className='valueProfile'><input type="text"  defaultValue={"yousef"}/></span></div>
+                <div><span className='keyProfile' >E-mail:</span><span className='valueProfile'> <input type="text" defaultValue={"yousef"}/></span></div>
+                <div><span className={'sendChangeProfile '+(showSaveBtn ? 'visible':'hidden')}><button>save</button></span></div>
             </div>
 
 
