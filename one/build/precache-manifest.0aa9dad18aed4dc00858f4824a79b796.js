4b0bcd2d19dc673afab86ead78ab11c6fcd69db2
youem1 <90083125+youem1@users.noreply.github.com>
2022-06-26 12:17:06 +0430

    w
    w

---
+++ one/build/precache-manifest.0aa9dad18aed4dc00858f4824a79b796.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d6abd4f216ee2300c85",
+    "url": "/static/css/main.873a7dc4.chunk.css"
+  },
+  {
+    "revision": "3d6abd4f216ee2300c85",
+    "url": "/static/js/main.3d6abd4f.chunk.js"
+  },
+  {
+    "revision": "36fa0ef07ba6a42e703f",
+    "url": "/static/js/1.36fa0ef0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0fd4e0949c5bf2c471ccecd185c51220",
+    "url": "/static/media/project-icon.0fd4e094.png"
+  },
+  {
+    "revision": "f056edb4ec6ecceba166e01b92fe0393",
+    "url": "/static/media/user.f056edb4.png"
+  },
+  {
+    "revision": "6bd94d9f5a55d2c07bfb50d8c063f2d9",
+    "url": "/static/media/task-icon.6bd94d9f.png"
+  },
+  {
+    "revision": "6d9139f87b79a320859da6dc144e2e33",
+    "url": "/static/media/Project-background.6d9139f8.jpg"
+  },
+  {
+    "revision": "871997d29d49536223ebb04fdf546ca0",
+    "url": "/static/media/send.871997d2.png"
+  },
+  {
+    "revision": "6f6bbb16aec97391aefe120ec5a4e6a2",
+    "url": "/static/media/user-profile.6f6bbb16.png"
+  },
+  {
+    "revision": "03a070dd5d6992ab91fd45fb4788c6ac",
+    "url": "/static/media/profile-bg (2).03a070dd.png"
+  },
+  {
+    "revision": "5a92970731948504aeedd4d8e8e81bf0",
+    "url": "/static/media/profile-bg.5a929707.jpg"
+  },
+  {
+    "revision": "47794afe8ad67c2c2bb8cc8d696ccd47",
+    "url": "/static/media/Group.47794afe.png"
+  },
+  {
+    "revision": "fb964616347bb449941d5b1353be9c77",
+    "url": "/index.html"
+  }
+];
